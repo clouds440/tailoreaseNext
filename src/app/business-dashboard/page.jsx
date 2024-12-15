41983cbaa418a0fca542c2f77c2db70a4b9b22f1
Zahid <zahidahmad440@gmail.com>
2024-12-15 13:34:03 +0500

    Displayed business name

--- src/app/business-dashboard/page.jsx
+++ src/app/business-dashboard/page.jsx
@@ -85,7 +85,7 @@ const BusinessDashboard = () => {
       className={`items-center p-6 mx-auto my-4 rounded-3xl max-w-[97%] max-h-[96%] h-[96%] overflow-hidden select-none justify-center text-3xl text-white flex ${theme.mainTheme}`}
     >
       {/* Business account is approved and active. Dashboard content displayed here */}
-      <div className="flex flex-col mr-3">Business Name:</div>
+      <div className="flex flex-col mr-3">{businessData.businessName}:</div>
       <div className="flex">Your business dashboard is not ready</div>
     </div>
   ) : (
