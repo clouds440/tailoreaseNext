e4f2272918a07107e331210d34b1d29f9672574b
Zahid <zahidahmad440@gmail.com>
2024-12-20 18:27:19 +0500

    Streamlined styles across the whole website

--- src/app/market/page.jsx
+++ src/app/market/page.jsx
@@ -7,7 +7,7 @@ const Market = () => {
 
   return (
     <div
-      className={`max-w-[99.5%] mx-auto items-center p-6 my-4 md:my-1 rounded-lg h-[96%] overflow-hidden select-none justify-center flex ${theme.mainTheme}`}
+      className={`max-w-[99.5%] mx-auto items-center p-6 my-4 md:my-1 rounded-lg h-screen overflow-hidden select-none justify-center flex ${theme.mainTheme}`}
     >
       <div className="flex text-3xl">Market is not ready</div>
     </div>
