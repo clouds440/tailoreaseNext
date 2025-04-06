f7ed2812fb2530802b65509bb9d3f40bea728f7e
Syed Ali Abbas Naqvi <aliabbasnaqvii51214@gmail.com>
2025-04-06 11:41:33 +0500

    Added the separate page for the Try On Page.

---
+++ src/app/try-on/page.jsx
@@ -0,0 +1,37 @@
+"use client";
+import { useContext } from "react";
+import UserContext from "@/utils/UserContext";
+import Image from "next/image";
+import Link from "next/link";
+const Market = () => {
+  const { theme } = useContext(UserContext);
+
+  return (
+    <div
+      className={`max-w-[99.5%] mx-auto items-center p-6 my-4 md:my-1 rounded-lg h-screen overflow-hidden select-none justify-center flex ${theme.mainTheme}`}
+    >
+      <Link href={"/outfit-customization?outfit=jacket"}>
+        <Image
+          className="max-w-full p-12 w-auto cursor-pointer border"
+          src="/thumbnails/jacket-thumbnail.png"
+          alt="This is the login Image."
+          width={100} // Specify the width
+          height={100} // Specify the height
+          priority // Optional: for high-priority images like login
+        />
+      </Link>
+      <Link href={"/outfit-customization?outfit=shirt"}>
+        <Image
+          className="max-w-full p-12 w-auto cursor-pointer border"
+          src="/thumbnails/shirt-thumbnail.png"
+          alt="This is the login Image."
+          width={100} // Specify the width
+          height={110} // Specify the height
+          priority // Optional: for high-priority images like login
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Market;
